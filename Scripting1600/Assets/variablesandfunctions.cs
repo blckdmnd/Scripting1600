e9dd3e70dcc79cbc2657571b0c60fbe3b523fdb8
Eddie Perez <california.97la@gmail.com>
2015-09-24 10:29:46 -0700

    adding basic syntax and variables and functions scripts

---
+++ Scripting1600/Assets/variablesandfunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variablesandfunctions : MonoBehaviour {
+    int myint = 5;
+
+    // Use this for initialization
+    void Start() {
+        myint = multiplybytwo(myint);
+        Debug.Log(myint);
+
+    }
+
+    // Update is called once per frame
+    int multiplybytwo(int number)
+    {     int ret;
+    ret = number* 2;
+        return ret;
+        }
+	
+	}
+
